d565465fcab24ba2154933ecb882c276a7f62d5e
timur.borisevich <timurborisevich@mail.ru>
2020-12-10 11:11:22 +0300

    _

--- guicorn.py
+++ guicorn.py
@@ -10,14 +10,11 @@ def pars_environ(environ):
         params_dic[cur_par[0]] = cur_par[1]
     return params_dic
 
-
-
 def app(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
     environ = unquote(environ['QUERY_STRING'])
     params_dic = pars_environ(environ)
     answer = DetectWorkType(params_dic['text'], params_dic['text'])
-    return [bytes(str(answer), encoding="utf8")]
     return [bytes(str(answer), encoding="utf8")]
 
 bind = "0.0.0.0:5000"
